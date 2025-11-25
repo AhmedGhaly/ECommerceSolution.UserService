cef65486de471ce21659e796091e9b1eec3d18da
AhmedGhaly <ahmedGhaly447@gmail.com>
2025-11-25 15:38:04 +0200

    add dependancy injection

--- ECommerce.Api/Program.cs
+++ ECommerce.Api/Program.cs
@@ -1,13 +1,31 @@
+using Ecommerce.Core;
+using ECommerce.Infrastructure;
+
 namespace ECommerce.Api
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+
+            
+
+
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddInfrastructure();
+            builder.Services.AddCore();
+
+
+            builder.Services.AddControllers();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseAuthentication();
+
+            app.MapControllers();
 
             app.Run();
         }
---
+++ Ecommerce.Core/DependancyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.Core
+{
+    public static class DependancyInjection
+    {
+        public static IServiceCollection AddCore(this IServiceCollection services) {
+
+            return services;
+        }
+    }
+}
